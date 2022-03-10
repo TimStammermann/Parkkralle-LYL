aa10cd8231e96a57721c7ebd571cf2f8bb14efef
TimStammermann <101358691+TimStammermann@users.noreply.github.com>
2022-03-10 20:19:53 +0100

    Update Config_vItems.hpp

--- Altis_Life.Altis/config/Config_vItems.hpp
+++ Altis_Life.Altis/config/Config_vItems.hpp
@@ -1,1 +1,10 @@
-
+class parkkralle {
+        variable = "parkkralle";
+        displayName = "STR_Item_Parkkralle";
+        weight = 1;
+        buyPrice = 10;
+        sellPrice = -1;
+        illegal = false;
+        edible = -1;
+        icon = "icons\parkkralle.paa";
+    };
